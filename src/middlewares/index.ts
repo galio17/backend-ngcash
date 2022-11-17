9a0a1f6e53becd752c565b85a96fa974d2efcafb
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-11-17 18:26:18 -0300

    feat: add token verification middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,3 @@
 export { errorHandlerMiddleware } from "./errorHandler.middleware";
 export { validateSchemaMiddleware } from "./validateSchema.middleware";
+export { verifyTokenMiddleware } from "./verifyToken.middleware";
